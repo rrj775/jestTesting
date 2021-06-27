a0da7b99d1f3e98b8bc0e02253a7beb13d61d652
rajat.raj <rrj775@gmail.com>
2021-06-27 09:15:27 +0530

    Testing with jest and enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+    adapter: new EnzymeAdapter(),
+    disableLifeCycleMethods: true
+})
